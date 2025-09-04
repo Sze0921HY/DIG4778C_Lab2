61add0bf7d17381b92fe93887a1e7d4749716ba4
Sze <sz982600@ucf.edu>
2025-09-03 20:35:57 -0400

    :/

---
+++ LabSubmission_2/Assets/Script/Testing.cs
@@ -0,0 +1,35 @@
+using System.Linq;
+using UnityEngine;
+using UnityEditor;
+
+public class Testing : MonoBehaviour
+{
+    // Just a placeholder MonoBehaviour
+}
+
+// Custom editor for Testing
+[CustomEditor(typeof(Testing)), CanEditMultipleObjects]
+public class TestingEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        EditorGUILayout.BeginHorizontal();
+
+        if (GUILayout.Button("Select all cubes/spheres"))
+        {
+            var allObjectsBehaviour = GameObject.FindObjectsOfType<Testing>();
+            var allGameObjects = allObjectsBehaviour.Select(enemy => enemy.gameObject).ToArray();
+
+            Selection.objects = allGameObjects;
+        }
+
+        if (GUILayout.Button("Clear selection"))
+        {
+            Selection.objects = new Object[] { (target as Testing).gameObject };
+        }
+
+        EditorGUILayout.EndHorizontal();
+    }
+}
